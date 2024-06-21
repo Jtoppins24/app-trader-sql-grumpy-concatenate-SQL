3c7861d582370af112997ba5fa2a8d8d3cb49d45
unknown <you@example.com>
2024-06-20 21:22:44 -0500

    working on presentation

--- scripts/erica.henley.app-trader.sql
+++ scripts/erica.henley.app-trader.sql
@@ -5,7 +5,8 @@ WITH cte AS (
 	FROM play_store_apps AS play
 	JOIN app_store_apps AS apple
 	USING(name)
-),
+	),
+	
 	cte_2 AS (
 	SELECT name,
 	(cte.avg_rating * 2 + 1) AS lifespan,
@@ -15,34 +16,38 @@ WITH cte AS (
 	END) AS app_price
 	FROM cte
 	JOIN app_store_apps AS apple
-	USING (name)
+	USING(name)
+	),
+	
+	cte_3 AS(
+	SELECT
+	name, CASE WHEN cte_2.app_price BETWEEN 0 AND 1 THEN 10000
+	ELSE cte_2.app_price *10000
+	END AS purchase_price
+	FROM cte_2
+	),
+	cte_4 AS(
+	SELECT name,
+	(cte_2.lifespan*12) * 10000 AS total_revenue,
+	(cte_2.lifespan*12) * 1000 + cte_3.purchase_price AS total_cost
+	FROM cte_2
+	JOIN cte_3
+	USING(name)
 	)
 	
 SELECT
 	DISTINCT(name),
 	cte.avg_rating,
-	cte_2.app_price,
-	CASE WHEN cte_2.app_price BETWEEN 0 AND 1 THEN 10000
-	ELSE cte_2.app_price *10000
-	END AS purchase_price,
--- purchase price defined as the initial cost to buy the rights based on the in-app price (max_in_app_price * 10k)
--- case statement here to make sure that we get 10ks for the 0-->1s
-	1000 AS monthly_cost,
--- monthly cost defined as the cost to continue the rights to app, should all be 10k bec we limited to only apps on both stores
-	cte_2.lifespan,
--- lifespan defined as the projected length the app will be popular/relevant based on its avg rating
--- might want to add an additional column or change this one to months so that it plays well with the payback column
-	cte_2.lifespan*12 AS lifespan_in_months,
-	10000 AS monthly_revenue,
---monthly_revenue defined as $10k per month bc it collects 5k per app
-	9000 AS contribution_margin,
--- contribution margin defined as the revenue - monthly cost (starts to indicate how quickly we will break even or how much profit is being contributed back per month to pack back the initial investment), again the same bc we have the same revenue and monthly cost for all apps joined in this table
-	'monthly_revenue * lifespan' AS total_revenue,
-	'monthly_cost * lifespan + purchase_price'AS total_cost,
-	'total_revenue - total_cost' AS profit,
-	(ROUND((cte_2.app_price * 10000) / 9000), 1) AS payback_in_months
--- payback defined as the purchase price/initial cost DIVIDED BY the contribution margin --> tells us exactly when we will break even
--- might need to fix this column so it's not a record but an integer or numeric data type....
+	cte_2.app_price :: money,
+	cte_3.purchase_price :: money,
+	1000 :: money AS monthly_cost,
+	(cte_2.lifespan*12) :: int AS lifespan_in_months,
+	10000 :: money AS monthly_revenue,
+	9000 :: money AS contribution_margin,
+	cte_4.total_revenue :: money,
+	cte_4.total_cost :: money,
+	(cte_4.total_revenue - cte_4.total_cost) :: money AS profit,
+	ROUND((cte_2.app_price * 10000) / 9000, 1)  AS payback_in_months
 FROM app_store_apps AS apple
 JOIN play_store_apps AS play
 USING(name)
@@ -50,4 +55,8 @@ JOIN cte
 	USING(name)
 JOIN cte_2
 	USING(name)
-ORDER BY cte.avg_rating DESC;
+JOIN cte_3
+	USING(name)
+JOIN cte_4
+	USING(name)
+ORDER BY avg_rating DESC;
